7703dfd974a97b2805c34d4813cdcd648a038a9d
Aaron Zhong <aaron_zhong2b@hotmail.com>
2016-11-23 16:28:11 +1300

    Add Branches datamodel and add Mobile.Client Nuget package

---
+++ ContosoBankBot/DataModels/Branches.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoBankBot.Models
+{
+    public class Branches
+    {   
+        [JsonProperty(PropertyName = "id")]
+        public string ID { get; set; }
+
+        [JsonProperty(PropertyName = "NAME")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "LOCATION")]
+        public string Location { get; set; }
+
+        [JsonProperty(PropertyName = "WEEKDAY_OPENING")]
+        public string WeekdayOpen { get; set; }
+
+        [JsonProperty(PropertyName = "WEEKDAY_CLOSING")]
+        public string WeekdayClose { get; set; }
+
+        [JsonProperty(PropertyName = "WEEKEND_OPENING")]
+        public string WeekendOpen { get; set; }
+
+        [JsonProperty(PropertyName = "WEEKEND_CLOSING")]
+        public string WeekendClose { get; set; }
+
+        [JsonProperty(PropertyName = "MANAGER")]
+        public string Manager { get; set; }
+    }
+}
