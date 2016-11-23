f0b0d391ab81ba55210116f5e9dc6f1eb9fc995a
Aaron Zhong <aaron_zhong2b@hotmail.com>
2016-11-23 23:08:04 +1300

    deploy bot to azure, registered with bot framework

---
+++ ContosoBankBot/DataModels/Atm_Machines.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoBankBot.DataModels
+{
+    public class Atm_Machines
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string ID { get; set; }
+
+        [JsonProperty(PropertyName = "LOCATION")]
+        public string Location { get; set; }
+
+        [JsonProperty(PropertyName = "AVAILABLE")]
+        public Boolean Available { get; set; }
+
+    }
+}
