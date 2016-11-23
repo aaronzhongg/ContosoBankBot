b3091a6ec2558c3a7e1e550194a460a3cb6ca40a
Aaron Zhong <aaron_zhong2b@hotmail.com>
2016-11-23 19:13:09 +1300

    add Branches, branch <branch name>, help commands

---
+++ ContosoBankBot/AzureManager.cs
@@ -0,0 +1,54 @@
+﻿using ContosoBankBot.Models;
+using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ContosoBankBot
+{
+    public class AzureManager
+    {
+
+        private IMobileServiceTable<Branches> branchesTable;
+        private static AzureManager instance;
+        private MobileServiceClient client;
+
+        private AzureManager()
+        {
+            this.client = new MobileServiceClient("http://phase2-az.azurewebsites.net");
+            this.branchesTable = this.client.GetTable<Branches>();
+        }
+
+        public MobileServiceClient AzureClient
+        {
+            get { return client; }
+        }
+
+        public static AzureManager AzureManagerInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AzureManager();
+                }
+
+                return instance;
+            }
+        }
+
+        public async Task<List<Branches>> GetBranches()
+        {
+            return await this.branchesTable.ToListAsync();
+        }
+
+        public async Task<Branches> GetBranch(String branch)
+        {
+            List<Branches> branches = await GetBranches();
+
+            return branches.First(b => b.Name.ToLower() == branch.ToLower());
+        }
+    }
+}
--- ContosoBankBot/Controllers/MessagesController.cs
+++ ContosoBankBot/Controllers/MessagesController.cs
@@ -7,6 +7,8 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using ContosoBankBot.Models;
+using System.Collections.Generic;
 
 namespace ContosoBankBot
 {
@@ -30,6 +32,28 @@ namespace ContosoBankBot
                 if (userMessage.ToLower().Equals("help"))
                 {
                     endOutput = "Avaliable commands: \n\n Branches - Return list of branches \n Branch <Branch name> - Return Branch information";
+                }
+                else if (userMessage.ToLower().Equals("branches"))
+                {
+                    List<Branches> branches = await AzureManager.AzureManagerInstance.GetBranches();
+                    endOutput = "";
+
+                    foreach (Branches b in branches)
+                    {
+                        endOutput += "\nBank Name: " + b.Name + "\nLocation: " + b.Location + "\n";
+                    }
+
+                }
+                else if (userMessage.Length > 6)
+                {
+                    if (userMessage.ToLower().Substring(0, 6).Equals("branch"))
+                    {
+                        string branch = userMessage.Substring(7);
+
+                        Branches b = await AzureManager.AzureManagerInstance.GetBranch(branch);
+                        endOutput = "Bank Name: " + b.Name + " \nLocation: " + b.Location + " \nWeekday Open Hours: " + b.WeekdayOpen + " - " + b.WeekdayClose + " \nWeekend Open Hours: " + b.WeekendOpen + " - " + b.WeekendClose;
+
+                    }
                 }
 
                 // return our reply to the user
