9b09e99ab4997d01a7eabd729c40d3306945bc92
Aaron Zhong <aaron_zhong2b@hotmail.com>
2016-11-24 18:57:40 +1300

    Refactor code

--- ContosoBankBot/Controllers/MessagesController.cs
+++ ContosoBankBot/Controllers/MessagesController.cs
@@ -10,6 +10,7 @@ using Newtonsoft.Json;
 using ContosoBankBot.Models;
 using System.Collections.Generic;
 using ContosoBankBot.DataModels;
+using Newtonsoft.Json.Linq;
 
 namespace ContosoBankBot
 {
@@ -30,21 +31,16 @@ namespace ContosoBankBot
                 StateClient stateClient = activity.GetStateClient();
                 BotData userData = await stateClient.BotState.GetUserDataAsync(activity.ChannelId, activity.From.Id);
 
-                var userMessage = activity.Text;
+                string[] userMessage = activity.Text.Split(' ');
+                string rawMessage = activity.Text;
                 var endOutput = "Welcome to Contoso Bank. Type a command or type Help for a list of available commands";
 
-                //if (userData.GetProperty<bool>("AdminRights"))
-                //{
-                //    Activity reply1 = activity.CreateReply("You have admin rights");
-                //    await connector.Conversations.ReplyToActivityAsync(reply1);
-                //}
-
-                if (userMessage.ToLower().Equals("help"))
+                if (userMessage[0].ToLower().Equals("help"))
                 {
                     endOutput = "Avaliable commands: \n\n Branches - Return list of branches \n Branch <Branch name> - Return Branch information  \n ATMS - Return a list of ATMS and their availablity ";
 
                 }
-                else if (userMessage.ToLower().Equals("branches"))
+                else if (userMessage[0].ToLower().Equals("branches"))
                 {
                     List<Branches> branches = await AzureManager.AzureManagerInstance.GetBranches();
                     endOutput = "";
@@ -55,7 +51,7 @@ namespace ContosoBankBot
                     }
 
                 }
-                else if (userMessage.ToLower().Equals("atms"))
+                else if (userMessage[0].ToLower().Equals("atms"))
                 {
                     List<Atm_Machines> atms = await AzureManager.AzureManagerInstance.GetATMs();
                     endOutput = "";
@@ -65,87 +61,111 @@ namespace ContosoBankBot
                         endOutput += "\nATM Location: " + a.Location + "\n Available: " + a.Available + "\n";
                     }
                 }
-                else if (userMessage.ToLower().Equals("login"))
-                {
-
-                }
-                else if (userMessage.ToLower().Equals("logout"))
+                else if (userMessage[0].ToLower().Equals("logout"))
                 {
                     userData.SetProperty<bool>("AdminRights", false);
                     await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
                     endOutput = "Successfully logged out";
                 }
-                else if (userMessage.Length > 6)
+                else if (userMessage[0].ToLower().Equals("branch"))
                 {
-                    if (userMessage.ToLower().Substring(0, 6).Equals("branch"))
+                    string branch = rawMessage.Substring(7);
+
+                    Branches b = await AzureManager.AzureManagerInstance.GetBranch(branch);
+                    endOutput = "Bank Name: " + b.Name + " \nLocation: " + b.Location + " \nWeekday Open Hours: " + b.WeekdayOpen + " - " + b.WeekdayClose + " \nWeekend Open Hours: " + b.WeekendOpen + " - " + b.WeekendClose;
+
+                }
+                else if (userMessage[0].ToLower().Equals("create-atm") && userData.GetProperty<bool>("AdminRights"))
+                {
+                    string atmLoc = rawMessage.Substring(11);
+
+                    Atm_Machines a = new Atm_Machines()
                     {
-                        string branch = userMessage.Substring(7);
+                        Location = atmLoc,
+                        Available = true
+                    };
 
-                        Branches b = await AzureManager.AzureManagerInstance.GetBranch(branch);
-                        endOutput = "Bank Name: " + b.Name + " \nLocation: " + b.Location + " \nWeekday Open Hours: " + b.WeekdayOpen + " - " + b.WeekdayClose + " \nWeekend Open Hours: " + b.WeekendOpen + " - " + b.WeekendClose;
+                    await AzureManager.AzureManagerInstance.AddAtm(a);
+                    endOutput = "Added new ATM at [" + atmLoc + "]";
+                }
+                else if (userMessage[0].ToLower().Equals("delete-atm") && userData.GetProperty<bool>("AdminRights"))
+                {
+                    string atmLoc = rawMessage.Substring(11);
 
+                    //Activity reply1 = activity.CreateReply(atmLoc);
+                    //await connector.Conversations.ReplyToActivityAsync(reply1);
+                    if (await AzureManager.AzureManagerInstance.DeleteAtm(atmLoc))
+                    {
+                        endOutput = "Deleted ATM [" + atmLoc + "]";
                     }
-                    else if (userMessage.ToLower().Substring(0, 10).Equals("create-atm") && userData.GetProperty<bool>("AdminRights"))
+                    else
                     {
-                        string atmLoc = userMessage.Substring(11);
-
-                        Atm_Machines a = new Atm_Machines()
-                        {
-                            Location = atmLoc,
-                            Available = true
-                        };
-
-                        await AzureManager.AzureManagerInstance.AddAtm(a);
-                        endOutput = "Added new ATM at [" + atmLoc + "]";
-                    }
-                    else if (userMessage.ToLower().Substring(0, 10).Equals("delete-atm") && userData.GetProperty<bool>("AdminRights"))
-                    {
-                        string atmLoc = userMessage.Substring(11);
-
-                        //Activity reply1 = activity.CreateReply(atmLoc);
-                        //await connector.Conversations.ReplyToActivityAsync(reply1);
-                        if (await AzureManager.AzureManagerInstance.DeleteAtm(atmLoc))
-                        {
-                            endOutput = "Deleted ATM [" + atmLoc + "]";
-                        }
-                        else
-                        {
-                            endOutput = "Could not delete ATM [" + atmLoc + "]. Please make sure ATM exists";
-                        }
-
-
-                    }
-                    else if (userMessage.ToLower().Substring(0, 10).Equals("update-atm") && userData.GetProperty<bool>("AdminRights"))
-                    {
-
-                    }
-
-                    if ((userMessage.ToLower().Substring(0, 10).Equals("update-atm") || userMessage.ToLower().Substring(0, 10).Equals("delete-atm") || userMessage.ToLower().Substring(0, 10).Equals("create-atm")) && !userData.GetProperty<bool>("AdminRights"))
-                    {
-                        endOutput = "Please login with an account with admin rights";
-                    }
-
-                    if (userMessage.Length > 5)
-                    {
-                        if (userMessage.ToLower().Substring(0, 5).Equals("login"))
-                        {
-                            string temp = userMessage.Substring(6);
-                            string[] userInfo = temp.Split(' ');
-
-                            endOutput = "Username: " + userInfo[0] + " Password: " + userInfo[1];
-
-                            if (await AzureManager.AzureManagerInstance.GetAccount(userInfo[0], userInfo[1]))
-                            {
-                                userData.SetProperty<bool>("AdminRights", true);
-                                await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
-                                endOutput = "Successfully logged in as " + userInfo[0];
-                            }
-                        }
+                        endOutput = "Could not delete ATM [" + atmLoc + "]. Please make sure ATM exists";
                     }
 
 
                 }
-               
+                else if (userMessage[0].ToLower().Equals("update-atm") && userData.GetProperty<bool>("AdminRights"))
+                {
+
+                }
+                else if (userMessage[0].ToLower().Equals("exchange-rate"))
+                {
+
+                    HttpClient client = new HttpClient();
+                    string x = await client.GetStringAsync(new Uri("http://api.fixer.io/latest?base=" + userMessage[1]));
+
+                    CurrencyObject.RootObject currencyObject = JsonConvert.DeserializeObject<CurrencyObject.RootObject>(x);
+                    //CurrencyObject.Rates ratesObject = JsonConvert.DeserializeObject<CurrencyObject.Rates>(x);
+
+                    //Activity reply1 = activity.CreateReply(exchangeRate);
+                    //await connector.Conversations.ReplyToActivityAsync(reply1);
+                }
+                else if ((userMessage[0].ToLower().Equals("update-atm") || userMessage[0].ToLower().Equals("delete-atm") || userMessage[0].ToLower().Equals("create-atm")) && !userData.GetProperty<bool>("AdminRights"))
+                {
+                    endOutput = "Please login with an account with admin rights";
+                }
+                else if (userMessage[0].ToLower().Equals("login"))
+                {
+                    if (await AzureManager.AzureManagerInstance.GetAccount(userMessage[1], userMessage[2]))
+                    {
+                        userData.SetProperty<bool>("AdminRights", true);
+                        await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
+                        endOutput = "Successfully logged in as " + userMessage[1];
+                    }
+                    else
+                    {
+                        endOutput = "Invalid username/password";
+                    }
+                }
+
+                else if (userMessage[0].ToLower().Equals("stock"))
+                {
+
+                    string stock = userMessage[1];
+                    try
+                    {
+
+
+                        HttpClient client = new HttpClient();
+                        var x = await client.GetStringAsync(new Uri("http://finance.google.com/finance/info?client=ig&q=" + stock));
+                        x = x.Replace("//", "");
+
+
+                        var v = JArray.Parse(x);
+
+                        foreach (var i in v)
+                        {
+                            var price = (decimal)i.SelectToken("l");
+                            endOutput = "The stock price for [" + stock + "] is $" + price;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        endOutput = "The stock [" + stock + "] could not be found";
+                    }
+                }
+
                 // return our reply to the user
                 Activity reply = activity.CreateReply(endOutput);
                 await connector.Conversations.ReplyToActivityAsync(reply);
---
+++ ContosoBankBot/Models/CurrencyObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoBankBot.Models
+{
+    public class CurrencyObject
+    {
+        public class RootObject
+        {
+            public string @base { get; set; }
+            //public Rates rates { get; set; }
+        }
+
+    }
+}
